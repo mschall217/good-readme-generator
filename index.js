6f20d3a718a141d3dae87df2b283d0d2604db3d6
Morgan Schall <morgan.allison.schall@gmail.com>
2021-03-23 22:26:01 -0400

    added structure, questions, set up function to write file

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+//what we use to get the data
+const fs = require('fs');
+//allows us to write files
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateREADME = (answers) =>
+  `document file structure goes here`
+
+// TODO: Create an array of questions for user input
+inquirer
+    .prompt([ 
+    {
+    type: 'input',
+    name: 'title',
+    message: 'Enter a title for your Prject',
+  },
+  {
+    type: 'input',
+    name: 'descripton',
+    message: 'Enter a description of your project',
+  },
+  {
+    type: 'input',
+    name: 'instructions',
+    message: 'Enter instructions to use your project',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information about your project',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Enter contribution guidelines about your project',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Enter test instructions for your project',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Which license would you like to use?',
+    choices: ['MIT', 'free'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please enter your github user name',
+  },
+]);
+.then((answers) =>{
+    const readMEPageContent = generateREADME(answers);
+    fs.writeFile('README.md', readMEPageContent, (err) =>
+    err ? console.log(err) : console.log('You wrote a read me!')
+    );
+});
 
 // TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+
