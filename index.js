1ca377d69849abe1f86ac03f87cb35f9054e813e
Morgan Schall <morgan.allison.schall@gmail.com>
2021-03-28 08:00:36 -0400

    fixed ignroed file got questions prompting

--- index.js
+++ index.js
@@ -1,69 +1,67 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 //what we use to get the data
-const fs = require('fs');
 //allows us to write files
 
-// TODO: Create a function to write README file
-const generateREADME = (answers) =>
-  `document file structure goes here`
-
-// TODO: Create an array of questions for user input
-inquirer
-    .prompt([ 
+const promptUser = () => {
+  return inquirer.prompt([
     {
-    type: 'input',
-    name: 'title',
-    message: 'Enter a title for your Prject',
-  },
-  {
-    type: 'input',
-    name: 'descripton',
-    message: 'Enter a description of your project',
-  },
-  {
-    type: 'input',
-    name: 'instructions',
-    message: 'Enter instructions to use your project',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'Enter usage information about your project',
-  },
-  {
-    type: 'input',
-    name: 'contribution',
-    message: 'Enter contribution guidelines about your project',
-  },
-  {
-    type: 'input',
-    name: 'test',
-    message: 'Enter test instructions for your project',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'Which license would you like to use?',
-    choices: ['MIT', 'free'],
-  },
-  {
-    type: 'input',
-    name: 'github',
-    message: 'Please enter your github user name',
-  },
-]);
-.then((answers) =>{
-    const readMEPageContent = generateREADME(answers);
-    fs.writeFile('README.md', readMEPageContent, (err) =>
-    err ? console.log(err) : console.log('You wrote a read me!')
-    );
-});
+      type: 'input',
+      name: 'title',
+      message: 'Enter a title for your Project',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project',
+    },
+    {
+      type: 'input',
+      name: 'instructions',
+      message: 'Enter instructions to use your project',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information about your project',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter information about any contributions to your project',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Enter test instructions for your project',
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'Please Select a license for your project',
+        choices: ['MIT', 'Free Public Licesnce', 'Apache License 2.0', 'this one']
+    },    
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github user name',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address',
+    },
+  ])
+  .then((answers) => {
+    console.log(answers)
+  });
+}
 
-// TODO: Create a function to initialize app
+const init = () => {
+  promptUser()
+}
 
-// Function call to initialize app
-init();
+init()
 
 
 
