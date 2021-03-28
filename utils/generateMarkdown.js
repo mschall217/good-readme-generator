c615db1dc86492ca64f6232e7af74e2f0f431f71
Morgan Schall <morgan.allison.schall@gmail.com>
2021-03-28 09:28:39 -0400

    got the license funciton working to produce 5 license for the user to pick from and puts them in the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,47 +11,53 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+function generateRepoName(title){
+  const repoName = title.split(' ').join("-")
+  return `${repoName}`;
+};
+
 function generateMarkdown(answers) {
-    `## ${answers.title}
+  const content = `## ${generateRepoName(answers.title)}
+
+  ${answers.description}
   
-    ${answers.description}
-    
-    ## Table of Contents
-    
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Test] (#test)
-    - [Support](#support)
-    - [Contributing](#contributing)
-    - [License] (#license)
-    
-    ## Installation
+  ## Table of Contents
   
-      ${answers.install}
-    
-    ## Usage
-    
-      ${answers.usage}
-    
-    ## Test
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Test] (#test)
+  - [Support](#support)
+  - [Contributing](#contributing)
+  - [License] (#license)
   
-      ${answers.test}
-    
-    ## Support
-    
-    Please [open an issue](https://github.com/${answers.github}/${answers.title}/issues/new) for support.
-    
-    ## Contributing
-    
-    ${answers.contributing}
-    
-    ## License
+  ## Installation
+
+    ${answers.install}
   
-    ${answers.license}
-    
-    ## Questions 
-    Visit my GitHub at ${answers.contact} or email me at ${answers.email} with any questions or concerns 
-    `
+  ## Usage
+  
+    ${answers.usage}
+  
+  ## Test
+
+    ${answers.test}
+  
+  ## Support
+  
+  Please [open an issue](https://github.com/${answers.github}/${answers.title}/issues/new) for support.
+  
+  ## Contributing
+  
+  ${answers.contributing}
+  
+  ## License
+
+  ${answers.license}
+  
+  ## Questions 
+  Visit my GitHub at ${answers.contact} or email me at ${answers.email} with any questions or concerns 
+  `
+  return content;
 }
 
 module.exports = generateMarkdown;
